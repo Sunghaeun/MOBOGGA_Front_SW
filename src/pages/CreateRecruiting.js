dcf708157f82810ec939f05fbfa5ee3c362aacb3
Haeun <sunghaeun1004@gmail.com>
2025-07-09 00:13:39 +0900

    feat : recruiting 생성 페이지 생성

---
+++ src/pages/CreateRecruiting.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./styles/Main.module.css";
+
+
+function CreateRecruiting() {
+  return (
+    <>
+      <div className={styles.main}>
+        main page
+      </div>
+    </>
+  );
+}
+
+export default CreateRecruiting;
