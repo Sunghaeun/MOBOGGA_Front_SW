644c6a115465da3a13139f9cf7ecc41e6895b351
lingardd09 <sherlockicker@gmail.com>
2025-09-08 15:38:47 +0900

    style: 푸터 디자인 반영

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,17 +2,18 @@ import React from "react";
 import styles from "./styles/Footer.module.css";
 import { useNavigate } from "react-router-dom";
 
-import moboggaLogo from "../assets/FooterLogo.svg";
-import likelion from "../assets/footer/likelion.png";
-import likelionhgu from "../assets/footer/likelionhgu.png";
-import insta from "../assets/instagram.svg";
+import moboggaLogo from "../assets/footer/footer_mobogga_logo.svg";
+import likelionLogo from "../assets/footer/footer_likelion_logo.svg";
+import likelionhguLogo from "../assets/footer/footer_likelionhgu_logo.svg";
+import instaLink from "../assets/footer/footer_insta_link.svg";
+import moboggaLink from "../assets/footer/footer_mobogga_link.svg";
 
 function Footer() {
   const navigate = useNavigate();
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.firstContainer}>
+      {/* <div className={styles.firstContainer}>
         <div
           style={{ cursor: "pointer" }}
           onClick={() => {
@@ -48,28 +49,32 @@ function Footer() {
         >
           문의
         </div>
-      </div>
+      </div> */}
 
       <div className={styles.secondContainer}>
         <div className={styles.logos}>
           <img
+            onClick={() =>
+              navigate("/")
+            }
+            src={moboggaLogo}
+            className={styles.logoImg}
+            alt=""
+          />
+          <div className={styles.verticalLine}></div>
+          <img
             onClick={() => window.open("https://likelion.net/", "_blank")}
-            src={likelion}
+            src={likelionLogo}
             className={styles.likelion}
             alt=""
           />
           <div className={styles.verticalLine}></div>
           <img
-            onClick={() => window.open("https://www.instagram.com/likelion_hgu/", "_blank")}
-            src={likelionhgu}
+            onClick={() =>
+              window.open("https://www.instagram.com/likelion_hgu/", "_blank")
+            }
+            src={likelionhguLogo}
             className={styles.likelionhgu}
-            alt=""
-          />
-          <div className={styles.verticalLine}></div>
-          <img
-            onClick={() => window.open("https://mobogga.netlify.app/faq", "_blank")}
-            src={moboggaLogo}
-            className={styles.logoImg}
             alt=""
           />
         </div>
@@ -78,27 +83,33 @@ function Footer() {
           탄생하였습니다.
         </div>
         <div className={styles.contact}>
-          <span>
-            <a href="tel:+821095438893">연락처: 010-9543-8893</a>
-          </span>{" "}
-          |{" "}
-          <span>
-            <a href="mailto:moboggahandong@gmail.com">
-              메일: moboggahandong@gmail.com
-            </a>
-          </span>
+          <span>연락처: 010-9543-8893</span> |{" "}
+          <span>메일: moboggahandong@gmail.com</span>
         </div>
 
-        <div>주소: 경상북도 포항시 북구 흥해읍 한동로 558, 한동대학교</div>
-        <div
-          className={styles.insta}
-          onClick={() =>
-            window.open("https://www.instagram.com/mobogga_handong/", "_blank")
-          }
-        >
-          <img src={insta} alt="" />
-          <span>instagram</span>
+        <div className={styles.address}>주소: 경상북도 포항시 북구 흥해읍 한동로 558, 한동대학교</div>
+        <div className={styles.links}>
+          <div
+            className={styles.mobogga}
+            onClick={() =>
+              navigate("/faq")
+            }
+          >
+            <img src={moboggaLink} alt="" />
+          </div>
+          <div
+            className={styles.insta}
+            onClick={() =>
+              window.open(
+                "https://www.instagram.com/mobogga_handong/",
+                "_blank"
+              )
+            }
+          >
+            <img src={instaLink} alt="" />
+          </div>
         </div>
+
         <span className={styles.copyRight}>
           Copyright © MOBOGGA. All rights reserved.
         </span>
