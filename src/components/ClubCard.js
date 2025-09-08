0d83b985ef17fa2962ed1826ed0046115850c273
lingardd09 <sherlockicker@gmail.com>
2025-09-08 16:39:33 +0900

    style: 테이프 위치 수정, 카드 사이즈 자동 조정,

--- src/components/ClubCard.js
+++ src/components/ClubCard.js
@@ -3,14 +3,19 @@ import styles from "./styles/ClubCard.module.css";
 
 import top from "../assets/main/topTape.svg";
 
-
-function ClubCard({ show , onClick }) {
+function ClubCard({ show, onClick }) {
   return (
     <>
       <div className={styles.clubCard} onClick={onClick}>
-        <img src={top} alt="" className={styles.top}/> 
-        <div className={styles.clubImg}>
-          <img src={show.poster} alt={show.clubName} className={styles.mainImg} />
+        <div className={styles.clubImg} style={{ position: "relative" }}>
+          <div className={styles.tape}>
+            <img src={top} alt="tape" />
+          </div>
+          <img
+            src={show.poster}
+            alt={show.clubName}
+            className={styles.mainImg}
+          />
         </div>
         <span className={styles.name}>{show.clubName}</span>
       </div>
