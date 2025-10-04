7a590d30e0fc69ae77d94ae376b61ac1efce6c8c
Haeun <sunghaeun1004@gmail.com>
2025-10-04 20:09:49 +0900

    feat : 공연 예매시 예매 불가 좌석 선택 모달 구현

--- src/pages/SeatTest.js
+++ src/pages/SeatTest.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 import styles from "./styles/SeatTest.module.css";
 
 import Seats from "../components/Seat/Seats";
+import SeatModal from "../components/Seat/SeatModal";
 
 function SeatTest() {
+  const [seatModalOpen, setSeatModalOpen] = useState(false);
+  const openSeatModal = () => setSeatModalOpen(true);
+  const closeSeatModal = () => {
+    setSeatModalOpen(false);
+    document.body.style.removeProperty("overflow");
+  };
+
   return (
     <>
       <div className={styles.main}>
+        <button onClick={openSeatModal}>test</button>
         <Seats />
       </div>
+      <SeatModal open={seatModalOpen} close={closeSeatModal} />
     </>
   );
 }
