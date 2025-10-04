f837c23673e8ddfecd86ddbb7e59e390a10576e0
Haeun <sunghaeun1004@gmail.com>
2025-10-04 20:42:49 +0900

    feat : 인덱스 번호 좌석번호로 변환하는 util 구현

--- src/pages/SeatTest.js
+++ src/pages/SeatTest.js
@@ -1,8 +1,9 @@
-import React,{useState} from "react";
+import React,{useState, useMemo} from "react";
 import styles from "./styles/SeatTest.module.css";
 
 import Seats from "../components/Seat/Seats";
 import SeatModal from "../components/Seat/SeatModal";
+import { idToCode, codeToId, findSeatByCode } from "../utils/seatUtils";
 
 function SeatTest() {
   const [seatModalOpen, setSeatModalOpen] = useState(false);
@@ -19,12 +20,19 @@ function SeatTest() {
     // 필요하면 여기서 closeSeatModal() 호출해도 됨 (모달 내부에서도 close 호출 중)
   };
 
+  const selectedSeatCodes = useMemo(
+    () => selectedSeatIds.map((id) => idToCode(id)).filter(Boolean),
+    [selectedSeatIds]
+  );
+
   return (
     <>
       <div className={styles.main}>
         <button onClick={openSeatModal}>test</button>
-        <h2>선택된 좌석은 바로바로 ~~</h2>
+        <h2>선택된 좌석의 인덱스 번호는 ~~</h2>
         <h3>{selectedSeatIds.join(", ")}</h3>
+        <h2>변환된 좌석 번호 !!</h2>
+        <h1>{selectedSeatCodes.join(", ")}</h1>
         <Seats />
       </div>
       <SeatModal open={seatModalOpen} close={closeSeatModal} onConfirm={handleConfirmFromModal}/>
