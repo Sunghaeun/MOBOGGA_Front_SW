6aa3ce7666b4f18101fafcc7b470d95c31f16b02
Haeun <sunghaeun1004@gmail.com>
2025-10-15 01:45:53 +0900

    feat : 예약 불가 좌석 모달 파일명 수정및 적용

--- src/pages/SeatTest.js
+++ src/pages/SeatTest.js
@@ -2,13 +2,23 @@ import React,{useState, useMemo} from "react";
 import styles from "./styles/SeatTest.module.css";
 
 import Seats from "../components/Seat/Seats";
+import NotReservationSeatModal from "../components/Seat/NotReservationSeatModal";
 import SeatModal from "../components/Seat/SeatModal";
 import { idToCode} from "../utils/seatUtils";
 
 function SeatTest() {
+  const [notReservationSeatModalOpen, setNotReservationSeatModalOpen] = useState(false);
   const [seatModalOpen, setSeatModalOpen] = useState(false);
   const [selectedSeatIds, setSelectedSeatIds] = useState([]);
 
+// 1) 예약 불가 좌석 지정 모달
+  const openNotReservationSeatModal = () => setNotReservationSeatModalOpen(true);
+  const closeNotReservationSeatModal = () => {
+    setNotReservationSeatModalOpen(false);
+    document.body.style.removeProperty("overflow");
+  };
+
+// 2) 예약 좌석 확인 모달
   const openSeatModal = () => setSeatModalOpen(true);
   const closeSeatModal = () => {
     setSeatModalOpen(false);
@@ -28,13 +38,15 @@ function SeatTest() {
   return (
     <>
       <div className={styles.main}>
-        <button onClick={openSeatModal}>test</button>
+        <button onClick={openNotReservationSeatModal}>예약불가 좌석 설정</button>
+        <button onClick={openSeatModal}>예약 좌석 위치</button>
         <h2>선택된 좌석의 인덱스 번호는 ~~</h2>
         <h3>{selectedSeatIds.join(", ")}</h3>
         <h2>변환된 좌석 번호 !!</h2>
         <h1>{selectedSeatCodes.join(", ")}</h1>
         <Seats />
       </div>
+      <NotReservationSeatModal open={notReservationSeatModalOpen} close={closeNotReservationSeatModal} onConfirm={handleConfirmFromModal}/>
       <SeatModal open={seatModalOpen} close={closeSeatModal} onConfirm={handleConfirmFromModal}/>
     </>
   );
